b0e98914cf5c284278a6cd463d3aa33166113368
Rory Maguire <rorymaguire00@gmail.com>
2022-01-18 12:11:04 +0000

    components created and user card started

--- FrontEnd/bootcampers-app/Components/UsersPage/index.js
+++ FrontEnd/bootcampers-app/Components/UsersPage/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import DisplayUserCard from "../DisplayUserCard";
 
 function UsersPage() {
+	const [users, setUsers] = setUsers([]);
+
 	return (
 		<div>
-			<DisplayUserCard />
+			{users.map((user) => (
+				<DisplayUserCard
+					city={users.city}
+					fullname={users.fullname}
+					nickname={users.nickname}
+				/>
+			))}
 		</div>
 	);
 }

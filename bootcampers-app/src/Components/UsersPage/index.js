89d102adafef278dfe2429eb71caa517c7300b5d
Rory Maguire <rorymaguire00@gmail.com>
2022-01-19 09:50:31 +0000

    searchbar attempted

--- bootcampers-app/src/Components/UsersPage/index.js
+++ bootcampers-app/src/Components/UsersPage/index.js
@@ -5,9 +5,17 @@ import DisplayUserCard from "../DisplayUserCard";
 function UsersPage() {
 	const [users, setUsers] = useState([]);
 
-	// const searchBar = document.getElementById("#search-bar");
+	// const searchBar = document.getElementById("searchBar");
+	// console.log(searchBar);
 
-	// searchBar.addEventListener("keyup", (e) => {});
+	// searchBar.addEventListener("keyup", (e) => {
+	// 	const searchedLetters = e.target.value.toLowerCase();
+	// 	console.log(searchedLetters);
+	// 	const filteredUsers = users.filter((user) => {
+	// 		return user.name.toLowerCase().includes(searchedLetters);
+	// 	});
+	// 	setUsers(filteredUsers);
+	// });
 
 	const getUsers = async () => {
 		const response = await fetch("http://localhost:4444/users");
@@ -30,8 +38,8 @@ function UsersPage() {
 
 	return (
 		<div className="users-page">
-			<div className="search-bar">
-				<input type="text" placeholder="bootcamper search..." id="search-bar" />
+			<div>
+				<input type="text" placeholder="bootcamper search..." id="searchBar" />
 			</div>
 			{users.map((user) => (
 				<DisplayUserCard
@@ -39,9 +47,11 @@ function UsersPage() {
 					fullname={user.fullname}
 					nickname={user.nickname}
 					briefIntro={
-						"blah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blah"
+						"hey everyone this is a simple test to show how the text will appear on the user profile cards - of course this is hard-coded and not dynamic but once we set up this functionality, it will be a message unique to everyone and what they choose to write on submission of their form "
 					}
-					image={user.profileImage}
+					profileImage={
+						"https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_1280.png"
+					}
 					userId={user.id}
 					deleteUser={deleteUser}
 				/>

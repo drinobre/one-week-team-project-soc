19bea85e2112839f31fbd6345bc644f9ce8d49af
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-01-19 13:47:27 +0000

    Adding extra info to to the inputs

--- bootcampers-app/src/Components/NewUserForm/index.js
+++ bootcampers-app/src/Components/NewUserForm/index.js
@@ -6,6 +6,15 @@ function NewUserForm() {
 	const [nickname, setNickname] = useState("");
 	const [city, setCity] = useState("");
 	const [hobbies, setHobbies] = useState("");
+	const [tvShows, setTvShows] = useState("");
+	const [favouriteArtists, setFavouriteArtists] = useState("");
+	const [favouriteFood, setFavouriteFood] = useState("");
+	const [superPower, setSuperPower] = useState("");
+	const [briefIntroduction, setBriefIntroduction] = useState("");
+	const [mostConfidentArea, setMostConfidentArea] = useState("");
+	const [improveKnowledge, setImproveKnowledge] = useState("");
+	const [favouriteQuote, setFavouriteQuote] = useState("");
+	
 
 	const submitForm = async (e) => {
 		e.preventDefault();
@@ -48,6 +57,62 @@ function NewUserForm() {
 					placeholder="Got any hobbies...?"
 					value={hobbies}
 					onChange={(e) => setHobbies(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="What are you favourite tv shows...?"
+					value={tvShows}
+					onChange={(e) => setTvShows(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="What are your favourite musical artists...?"
+					value={favouriteArtists}
+					onChange={(e) => setFavouriteArtists(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="What is your favourite food...?"
+					value={favouriteFood}
+					onChange={(e) => setFavouriteFood(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="What is your superpower...?"
+					value={superPower}
+					onChange={(e) => setSuperPower(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="Tell us a little about yourself"
+					value={briefIntroduction}
+					onChange={(e) => setBriefIntroduction(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="In what coding language are you most confident...?"
+					value={mostConfidentArea}
+					onChange={(e) => setMostConfidentArea(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="In what coding language would you like to improve your knowledge...?"
+					value={improveKnowledge}
+					onChange={(e) => setImproveKnowledge(e.target.value)}
+				/>
+
+				<input
+					type="text"
+					placeholder="What is your favourite quote...?"
+					value={favouriteQuote}
+					onChange={(e) => setFavouriteQuote(e.target.value)}
 				/>
 				<button className="btn btn-success">Add</button>
 			</form>
